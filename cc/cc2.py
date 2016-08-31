a1bd97777e084dbb974f3e2a4039baf1083dc068
Alexander Schäfer <alexander.schaefer@uni-duesseldorf.de>
2016-08-31 16:58:08 +0200

    Add second test data record

--- cc/cc2.py
+++ cc/cc2.py
@@ -19,36 +19,83 @@ renderer = pystache.Renderer(
 	file_encoding = 'utf-8'
 )
 
+# data = {
+# 	"res_path": u"../res",
+# 	"css_path": u"../style/cc.css",
+# 	"first_name": u"Alexander",
+# 	"last_name": u"Schäfer",
+# 	"company_name": u"HHC Düsseldorf",
+# 	"content": u"""Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+# Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+
+# At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.""",
+# 	"comments": [
+# 		{
+# 			"first_name": u"Kundendienst",
+# 			"last_name": u"",
+# 			"content": u"""Guten Tag,
+
+# das können wir so nicht bestätigen!""",
+# 			"answers": [
+# 				{
+# 					"first_name": u"Alexander",
+# 					"last_name": u"Schäfer",
+# 					"content": u"""Antwort1!<img src="{{res_path}}/profile/dummy.png />" """
+# 				},
+# 				{
+# 					"first_name": u"Alexander",
+# 					"last_name": u"Schäfer",
+# 					"content": u"""Antwort 2
+
+
+# Geht auch hier noch weiter :-)"""
+# 				},
+# 				{
+# 					"first_name": u"Alexander",
+# 					"last_name": u"Schäfer",
+# 					"content": u"""Antwort    3"""
+# 				}
+# 			]
+# 		},
+# 		{
+# 			"first_name": u"Kundendienst",
+# 			"last_name": u"",
+# 			"content": u"""Guten Tag,
+
+# das können wir so nicht bestätigen!""",
+# 			"answers": [
+# 				{
+# 					"first_name": u"Alexander",
+# 					"last_name": u"Schäfer",
+# 					"content": u"""Fieß!"""
+# 				}
+# 			]
+# 		}
+# 	]
+# }
+
 data = {
 	"res_path": u"../res",
 	"css_path": u"../style/cc.css",
-	"first_name": u"Alexander",
-	"last_name": u"Schäfer",
-	"company_name": u"HHC Düsseldorf",
-	"content": u"""Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
-
-At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.""",
+	"first_name": u"0",
+	"last_name": u"1",
+	"company_name": u"2",
+	"content": u"""1""",
 	"comments": [
 		{
-			"first_name": u"Kundendienst",
+			"first_name": u"1",
 			"last_name": u"",
-			"content": u"""Guten Tag,
-
-das können wir so nicht bestätigen!""",
+			"content": u"""x""",
 			"answers": [
 				{
-					"first_name": u"Alexander",
-					"last_name": u"Schäfer",
-					"content": u"""Antwort1!"""
+					"first_name": u"x",
+					"last_name": u"y",
+					"content": u"""yz"""
 				},
 				{
-					"first_name": u"Alexander",
-					"last_name": u"Schäfer",
-					"content": u"""Antwort 2
-
-
-Geht auch hier noch weiter :-)"""
+					"first_name": u"v",
+					"last_name": u"r",
+					"content": u""":-)"""
 				},
 				{
 					"first_name": u"Alexander",

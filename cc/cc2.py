844393e71b7ea0d2fc23fc1d684121b1cfdd3046
Alexander Schäfer <alexander.schaefer@uni-duesseldorf.de>
2016-11-12 19:15:53 +0100

    Add option blur_last_name

--- cc/cc2.py
+++ cc/cc2.py
@@ -3,6 +3,8 @@
 
 import pystache
 from os.path import join as OS_join
+import sys
+import json
 
 class PartialsLoader(object):
 
@@ -19,68 +21,6 @@ renderer = pystache.Renderer(
 	file_encoding = 'utf-8'
 )
 
-data = {
-	"res_path": u"../res",
-	"css_path": u"../style/cc.css",
-	"output_path": u"../output",
-	"first_name": u"Alexander",
-	"last_name": u"Schäfer123123",
-	"image": u"dummy",
-	"blur_image": True,
-	"company_name": u"HHC Düsseldorf",
-	"content": u"""!"§$%&/()=?*'ÄPÖ_:;:_Ä'*`?=)(/&%$§"¶¢[]{}≠¿•æœø∆∞µ~∫ƒ∂®†Ω""",
-	"comments": [
-		{
-			"first_name": u"Kundendienst",
-			"last_name": u"",
-			"image": u"dummy",
-			"blur_image": False,
-			"content": u"""Guten Tag,
-
-das können wir so nicht bestätigen!""",
-			"answers": [
-				{
-					"first_name": u"Alexander",
-					"last_name": u"Schäfer1231",
-					"image": u"dummy",
-					"blur_image": True,
-					"content": u"""Antwort1!"""
-				},
-				{
-					"first_name": u"Kundendienst",
-					"image": u"dummy",
-					"blur_image": False,
-					"last_name": u"Schäfer",
-					"content": u"""Antwort 2 Geht auch hier noch weiter :-)"""
-				},
-				{
-					"first_name": u"Alexander",
-					"last_name": u"Schäfer",
-					"image": u"dummy",
-					"blur_image": True,
-					"content": u"""Antwort    3"""
-				}
-			]
-		},
-		{
-			"first_name": u"Peter",
-			"last_name": u"Maffay",
-			"image": u"dummy",
-			"blur_image": True,
-			"content": u"Fake!",
-			"answers": [
-				{
-					"first_name": u"Alexander",
-					"last_name": u"Schäfer",
-					"image": u"dummy",
-					"blur_image": False,
-					"content": u"""Fieß!"""
-				}
-			]
-		}
-	]
-}
-
 def encodeNewLines(string):
 	return "<br />".join(string.split("\n"))
 
@@ -91,9 +31,16 @@ def encodeNewLinesFromData(data):
 		for answer in comment["answers"]:
 			answer["content"] = encodeNewLines(answer["content"])
 
-encodeNewLinesFromData(data)
+path = sys.argv[1]
+with open(path, "r") as json_file:
+	json_file_text = json_file.read().replace("\\n", "<br>")
+	print json_file_text
+	json_file = json.loads(json_file_text)
 
-html = renderer.render_name('cc-post', data)
-f = open(OS_join(data["output_path"], "output.html"), "w")
-f.write(html.encode("UTF-8"))
-f.close()
+
+	#encodeNewLinesFromData(data)
+
+	html = renderer.render_name('cc-post', json_file)
+	f = open(OS_join(json_file["output_path"], "output.html"), "w")
+	f.write(html.encode("UTF-8"))
+	f.close()
